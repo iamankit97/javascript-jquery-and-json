f36e0a1f8d3b93e06d0192b60ee667d9e98accda
iamankit97 <31159906+iamankit97@users.noreply.github.com>
2021-02-13 21:37:09 +0530

    Update login.php

--- Assignments/Week_1/login.php
+++ Assignments/Week_1/login.php
@@ -40,7 +40,7 @@ if (isset($_POST['pass']) && isset($_POST['email'])) {
 <html>
 <head>
     <?php require_once "bootstrap.php"; ?>
-    <title>Welcome to Autos Database | Jared Best</title>
+    <title>Welcome to Autos Database | Ankit Gupta</title>
 </head>
 <body>
 <div class="container">
